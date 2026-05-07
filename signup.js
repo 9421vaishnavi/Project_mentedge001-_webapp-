999eaeb02aa8bc880a20dcf9d3d3a5b4d21ffc0a
9421vaishnavi <anantwalvaishnavi820@gmail.com>
2026-05-07 23:10:51 +0530

    Update signup.js(Signup with Google OAuthentication )
    implemented Google OAuth Authentication for user signup functionality.

--- signup.js
+++ signup.js
@@ -21,3 +21,10 @@ function validateAndSignup() {
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
+function handleCredentialResponse(response) {
+            console.log("JWT Token: " + response.credential);
+
+            const data = JSON.parse(atob(response.credential.split('.')[1]));
+            console.log("Name:", data.name);
+            console.log("Email:", data.email);
+        }
